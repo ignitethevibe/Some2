c2c3e3f5afe2d34917d29cc5dacde423995800ac
ignitethevibe <ignite.the.vibe@gmail.com>
2017-08-23 16:42:02 -0500

    Add files via upload

---
+++ some2/levels.py
@@ -0,0 +1,176 @@
+import pygame
+
+import constants
+import platforms
+from player import Bullet
+
+
+class Level():
+    # generic super class used to define level
+
+    def __init__(self, player):
+        # Constructor. Pass in a handle to player. needed for when collides
+
+        # Lists of sprites used in all levels
+        # Add or Remove Lists needed for game
+        self.platform_list = None
+        self.enemy_list = None
+
+        self.char_list = None
+
+        # Background Image
+        self.background = None
+
+        # How far this world has been scrolled to left/right
+        self.world_shift = 0
+        self.level_limit = -1000
+        self.platform_list = pygame.sprite.Group()
+        self.enemy_list = pygame.sprite.Group()
+        self.bullet_list = pygame.sprite.Group()
+        self.player = player
+        self.bullet = Bullet(player.rect.x, player.rect.y)
+
+
+    # update everything on this level
+    def update(self):
+        self.platform_list.update()
+        self.enemy_list.update()
+        self.bullet_list.update()
+
+
+    def draw(self, screen):
+        # draw everything on this level
+        # don't shift the background as much as the sprites are shifted
+        # to give a feeling of depth
+        screen.fill(constants.blue)
+        screen.blit(self.background, (self.world_shift // 3,0))
+
+        self.platform_list.draw(screen)
+        self.enemy_list.draw(screen)
+        self.bullet_list.draw(screen)
+
+
+    def shift_world(self, shift_x):
+        # when the user moves left/right scroll everything
+
+        # keep track of shift amount'
+        self.world_shift += shift_x
+
+        # Go through all the sprite lists and shift
+        for platform in self.platform_list:
+            platform.rect.x += shift_x
+
+        for enemy in self.enemy_list:
+            enemy.rect.x += shift_x
+
+        for bullet in self.bullet_list:
+            bullet.rect.x += shift_x
+
+
+# Create platforms for the Level
+class Level_01(Level):
+    # Definiton for Level 1
+
+    def __init__(self, player):
+        # create level 1
+
+        # call the constructor
+        Level.__init__(self, player)
+
+        self.background = pygame.image.load('cityscape.png').convert()
+        self.background.set_colorkey(constants.white)
+        self.level_limit = -4000
+        bullet = Bullet(player.rect.x, player.rect.y)
+        self.bullet_list.add(bullet)
+
+        # Array with type of platform, and x, y location of the platform
+        level = [[platforms.METAL_BLOCK1, 420, 565],
+                [platforms.METAL_BLOCK3, 465, 565],
+                [platforms.METAL_BLOCK2, 510, 565],
+                [platforms.BIG_BLOCK_RIGHT, 555, 530],
+                [platforms.BIG_BLOCK_LEFT, 626, 530],
+                [platforms.GRINDO, 765, 395],
+                [platforms.BLOCK_O_TOP, 905, 395],
+                [platforms.BLOCK_O_TOP, 948, 395],
+                [platforms.REC_O_TOP1, 1025, 370],
+                [platforms.REC_O_TOP1, 1165, 315],
+                # G-Blocks
+                [platforms.BIG_BLOCK, 935, 509],
+                [platforms.LIGHT_NEON_BLOCK, 1021, 509],
+                [platforms.M_COLUMN, 1055, 572],
+                [platforms.LARGE_COLUMN, 1295, 464],
+                [platforms.SMILEY, 1294, 412],
+                #
+                [platforms.BIG_BLOCK_RIGHT2, 1705, 530],
+                [platforms.BIG_BLOCK_RIGHT, 1705, 460],
+                [platforms.BIG_BLOCK_LEFT2, 1776, 530],
+                [platforms.BIG_BLOCK_LEFT, 1776, 460],
+                [platforms.BIG_PIPE_BLOCK, 1730, 373],
+                # - Continuous Path and add enemies
+                [platforms.REC_O_TOP1, 1847, 555],
+                [platforms.REC_O_TOP3, 1944, 555],
+                [platforms.REC_O_TOP2, 2032, 555],
+                [platforms.REC_O_TOP3, 2126, 555],
+                [platforms.REC_O_TOP1, 2214, 555],
+                [platforms.REC_O_TOP1, 2311, 555],
+                [platforms.REC_O_TOP1, 2408, 555],
+                [platforms.BLOCK_O_LEFT, 2501, 525],
+                [platforms.REC_O_TOP2, 2548, 525],
+                [platforms.REC_O_TOP1, 2642, 525],
+                [platforms.REC_O_TOP1, 2739, 525],
+                [platforms.REC_O_TOP1, 2832, 525],
+
+                    ]
+
+        # Go through array above and add platforms
+        for platform in level:
+            block = platforms.Platform(platform[0])
+            block.rect.x = platform[1]
+            block.rect.y = platform[2]
+            block.player = self.player
+            self.platform_list.add(block)
+
+        # Add a custom moving platform
+        block = platforms.MovingPlatform(platforms.METAL_RECT1)
+        block.rect.x = 1350
+        block.rect.y = 280
+        block.boundary_left = 1350
+        block.boundary_right = 1600
+        block.change_x = 1
+        block.player = self.player
+        block.level = self
+        self.platform_list.add(block)
+
+
+# add Level 2
+class Level_02(Level):
+    def __init__(self, player):
+        Level.__init__(self, player)
+
+        self.background = pygame.image.load('pokeclouds.png').convert()
+        self.background.set_colorkey(constants.white)
+        self.level_limit = -1000
+
+        level = [[platforms.METAL_BLOCK3,0, 550 ],
+                [platforms.METAL_BLOCK1, 132, 550],
+                [platforms.METAL_BLOCK2, 264, 550],
+                ]
+
+
+        for platform in level:
+            block = platforms.Platform(platform[0])
+            block.rect.x = platform[1]
+            block.rect.y = platform[2]
+            block.player = self.player
+            self.platform_list.add(block)
+
+        # moving
+        block = platforms.MovingPlatform(platforms.METAL_RECT_THIN1)
+        block.rect.x = 1500
+        block.rect.y = 300
+        block.boundary_top = 100
+        block.boundary_bottom = 550
+        block.change_y = -1
+        block.player = self.player
+        block.level = self
+        self.platform_list.add(block)
---
+++ some2/platforms.py
@@ -0,0 +1,111 @@
+# - module for managing platforms
+
+import pygame
+from spritesheet_functions import SpriteSheet
+
+# These constants define our platform types:
+# -- Name of file
+# -- X Location of sprite
+# -- Y location of sprite
+# -- Width of sprite
+# -- Height of Sprite
+
+# ---- bottom right sprites ---
+# -- Blocks and Rectangles
+METAL_BLOCK1        = (1680, 1030, 45, 45)
+METAL_BLOCK2        = (1727,1032,45,45)
+METAL_BLOCK3        = (1725, 1077, 45, 45)
+GEAR_BLOCK1         = (1613, 597,41,39) # orange patterned
+GRINDO              = (1497, 887, 89, 48)
+BIG_BLOCK           = (1591, 986, 86,91)
+BIG_BLOCK_RIGHT     = (1864, 1033, 71, 70)
+BIG_BLOCK_LEFT      = (1974, 1032, 71, 70)
+BIG_BLOCK_RIGHT2    = (1864, 1139, 71, 70)
+BIG_BLOCK_LEFT2     = (1976, 1142, 71, 70)
+BLOCK_O_TOP         = (1635, 1168, 43 ,48) # 0_TOP = Orange Top
+REC_O_TOP1          = (1678, 964 ,97, 19)
+REC_O_TOP2          = (1772, 962, 94, 24)
+REC_O_TOP3          = (1953,1009,88,20)
+BLOCK_O_LEFT        = (1704, 639, 47, 50)
+
+SOLID_M_COLUMN      = (1637,893,38,92)
+
+# -- Graphic Blocks
+BRIGHT_NEON_BLOCK   = (1405, 1215, 88, 66)
+LIGHT_NEON_BLOCK    = (1589, 1216, 88, 66)
+M_COLUMN            = (1598,763,30,48)
+LARGE_COLUMN        = (1843, 734, 41, 136)
+SMILEY              = (1473, 592, 44, 52)
+BIG_PIPE_BLOCK      = (1314, 940, 92, 87)
+
+# -- For Moving
+
+METAL_RECT1         = (1728, 760 ,89,39)
+METAL_RECT_THIN1    = (1728, 802, 87, 18)
+M_SHAPED            = (1406, 1017, 84, 59)
+
+# ---             -----
+
+
+
+class Platform(pygame.sprite.Sprite):
+
+    def __init__(self, sprite_sheet_data):
+        super(Platform, self).__init__()
+
+        sprite_sheet = SpriteSheet("some_tiles.png")
+        # grab the image for this platform
+        self.image = sprite_sheet.get_image(sprite_sheet_data[0],
+                                            sprite_sheet_data[1],
+                                            sprite_sheet_data[2],
+                                            sprite_sheet_data[3])
+        self.rect = self.image.get_rect()
+
+class MovingPlatform(Platform):
+    # fancy platform that can move
+    def __init__(self, sprite_sheet_data):
+        super(MovingPlatform, self).__init__(sprite_sheet_data)
+
+        self.change_x = 0
+        self.change_y = 0
+
+        self.boundary_top = 0
+        self.boundary_bottom = 0
+        self.boundary_left = 0
+        self.boundary_right = 0
+
+        self.level = None
+        self.player = None
+
+    def update(self):
+        # if player is in the way it will shove player out of the way
+
+        # move left/right
+        self.rect.x += self.change_x
+
+        # See if hit player
+        hit = pygame.sprite.collide_rect(self, self.player)
+        if hit:
+            if self.change_x < 0:
+                self.player.rect.right = self.rect.left
+            else:
+                self.player.rect.left = self.rect.right
+
+        # move up/down
+        self.rect.y += self.change_y
+
+        # check for collision
+        hit = pygame.sprite.collide_rect(self, self.player)
+        if hit:
+            if self.change_y < 0:
+                self.player.rect.bottom = self.rect.top
+            else:
+                self.player.rect.top = self.rect.bottom
+
+        # check boundaries if need to reverse movement
+        if self.rect.bottom > self.boundary_bottom or self.rect.top < self.boundary_top:
+            self.change_y *= -1
+
+        cur_pos = self.rect.x - self.level.world_shift
+        if cur_pos < self.boundary_left or cur_pos > self.boundary_right:
+            self.change_x *= -1
